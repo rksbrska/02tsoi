de494d2bf2d6bcd8964b81c273e5d3f8fe3d538f
rksbrska <91909794+rksbrska@users.noreply.github.com>
2021-10-26 22:51:27 +0300

    Create extask03-b.c

---
+++ extask03-b.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  float x0, y0;
+  printf("x0, y0: ");
+  scanf("%f %f", &x0, &y0);
+  
+  float v0, a;
+  printf("v0, a: ");
+  scanf("%f %f", &v0, &a);
+  
+  float ra = a * (float) M_PI / 180.0f;
+  
+  float v0x = cosf(ra) * v0;
+  float v0y = sinf(ra) * v0;
+  
+  #define g 9.8f
+  //float x = x0 + v0x * t;
+  //float y = y0 + v0y * t - g * t * t / 2.0f;
+  
+  float d = v0y * v0y + 4.0f * g / 2.0f * y0;
+  printf("d = %f\n", d);
+  
+  float t1 = (-v0y - sqrtf(d)) / 2.0f * (-g / 2.0f);
+  float t2 = (-v0y + sqrtf(d)) / 2.0f * (-g / 2.0f);
+  printf("t1 = %f, t2 = %f\n", t1, t2);
+         
+  float t = 0.0f;
+  #define eps 0.001f
+  while (1)
+  {float x = x0 + v0x
+return 0;
+}
