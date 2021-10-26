fa65e0b663ed74573f134cba0e8f693c28c51356
rksbrska <91909794+rksbrska@users.noreply.github.com>
2021-10-26 23:46:22 +0300

    Create extask02-a.c

---
+++ extask02-a.c
@@ -0,0 +1,53 @@
+#define max_lastname 50
+#define num_marks 3
+#define mark_min 2
+#define mark_max 5
+
+struct element
+{
+  char lastbane[max_lastname];
+  int marks[num_marks];
+  
+  struct element *prev;
+  struct element *next;
+};
+element *first = NULL;
+element *last = NULL;
+
+int main()
+{
+  FILE *f = fopen("extask02-a.txt", "r");
+  while (1)
+  {
+    char lastname[max_lastname];
+    if (fscanf(f, "%s", lastname) != 1) break;
+    
+    struct  element *e = (struct element *) malloc(sizeof(struct e]));
+    if (last == NULL)
+    {
+      e->prev - NULL;
+      first = e;
+    }
+    else e-> prev = last;
+    e->next = NULL;
+    last = e;
+    
+    strcpy(e->lastname, lastname);
+    for (int i = 0; i < num_marks; i++)
+      e->marks[i] = mark_min + rand() % (mark_max - mark_min + 1)
+    }
+  fclose(f);
+  
+  struct element *current = first;
+  while (current != NULL)
+  { printf("%s", *current = first;
+           while (current != NULL)
+           {
+             printf("%s", current->lastname);
+             for (int i = 0; i < num_marks; i++)
+               printf(" %d", current->marks[i]);
+             printf("\n");
+             current = current->next;
+           }
+           return 0;
+           }
