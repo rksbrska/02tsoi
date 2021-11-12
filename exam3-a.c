60e90ea76595fd742b83f07dc3e62c6e876bc222
rksbrska <91909794+rksbrska@users.noreply.github.com>
2021-11-12 21:57:10 +0300

    Update exam3-a.c

--- exam3-a.c
+++ exam3-a.c
@@ -4,37 +4,37 @@
 
 int main(int argc, char **argv)
 {
+	int N, i;
+	int *a;
+	float avggrd;
+	
+	printf("Количество оценок аттестата:");
+	scanf("%d", &N);
+	a = (int*)malloc(N * sizeof(int));
 
-int N, i;
-int *a;
-printf("Введите количество оценок: ");
-scanf("%d", &N);
-a = (int*)malloc(N * sizeof(int));
-
-for (i = 0; i < N; i++)
+	for (i = 0; i < N; i++)
 	{
 		a[i] = 2 + rand() % (5 - 2 + 1);
 		printf("%d \n", a[i]);
 	}
 
-int sum = 0;
-for (i = 0; i < N; i++)
+	int sum = 0;
+	for (i = 0; i < N; i++)
 	{
-		sum = sum + a[i];
+		sum += a[i];
 	}
 
-float sred;
-sred = sum / N;
-printf("Средний балл аттестата: %.1f\n", sred);
+	avggrd = sum / N;
+	printf("Средний балл аттестата: %.1f\n", avggrd);
 
-if(sred >= 4.6)
+	if (avggrd > 4.6)
 	{
 		printf("Ученик поступит в университет\n");
 	}
-else
+	else
 	{
 		printf("Ученик не поступит в университет\n");
 	}
-
+	
 return 0;
 }
