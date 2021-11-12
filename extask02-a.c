3f593eb9a7bbb11cc4b43b9be0b25e33158d88f7
rksbrska <91909794+rksbrska@users.noreply.github.com>
2021-11-12 21:14:14 +0300

    Update extask02-a.c

--- extask02-a.c
+++ extask02-a.c
@@ -1,12 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
-
-
-
-
-
 #define max_lastname 50
 #define num_marks 3
 #define mark_min 2
@@ -16,14 +10,12 @@ struct element
 {
   char lastbane[max_lastname];
   int marks[num_marks];
-  
   struct element *prev;
   struct element *next;
 };
 
 struct element *first = NULL;
 struct element *last = NULL;
-
 
 int main()
 {
@@ -32,51 +24,48 @@ int main()
   {
     char lastname[max_lastname];
     if (fscanf(f, "%s", lastname) != 1) break;
-    
     struct  element *e = (struct element *) malloc(sizeof(struct element));
-    if (last == NULL)
-    {
-      e->prev - NULL;
-      e->next = NULL;
-      first = e;
-      last = e;
-    }
-    else 
-    {
-      last->next = e;
-      e-> prev = last;
-      e->next = NULL;
-      last = e;
-    }
-    
-    strcpy(e->lastname, lastname);
+
+     if (last == NULL)
+      {
+        e->prev - NULL;
+        e->next = NULL;
+        first = e;
+        last = e;
+      }
+       else 
+        {
+         last->next = e;
+         e-> prev = last;
+         e->next = NULL;
+         last = e;
+        }
+
+strcpy(e->lastname, lastname);
     for (int i = 0; i < num_marks; i++)
       e->marks[i] = mark_min + rand() % (mark_max - mark_min + 1)
     }
-  fclose(f);
-  
-  struct element *current = first;
+fclose(f);
+struct element *current = first;
+
   while (current != NULL)
-  { 
-    printf("%s", *current = first;
-             for (int i = 0; i < num_marks; i++)
-               printf(" %d", current->marks[i]);
-             printf("\n");
-             current = current->next;
-           }
-           printf("\n");
-           
-           
-           
-           current = last;
-           while (current != NULL)
-           {
-             printf("%s", current->lastname);
-             for (int i = 0; i <num_marks[i]);
-             printf(" %d", current->marks[i]);
-             printf("\n");
-             
-             current = current->prev;
-           }
-           return 0;
-           }
+   { 
+     printf("%s", *current -> lastname);
+     for (int i = 0; i < num_marks; i++)
+     printf(" %d", current->marks[i]);
+     printf("\n");
+     current = current->next;
+   }
+printf("\n");
+current = last;
+
+while (current != NULL)
+  {
+   printf("%s", current->lastname);
+   for (int i = 0; i <num_marks[i]);
+   printf(" %d", current->marks[i]);
+   printf("\n");
+   current = current->prev;
+  }
+return 0;
+}
