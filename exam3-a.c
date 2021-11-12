554b9dda43e59da35f082b101e018dcffdfc435f
rksbrska <91909794+rksbrska@users.noreply.github.com>
2021-11-12 22:16:34 +0300

    Update exam3-a.c
    ОТЛИЧНАЯ РАБОТА

--- exam3-a.c
+++ exam3-a.c
@@ -2,32 +2,40 @@
 #include <stdlib.h>
 #include <malloc.h>
 
+	float faverage(int x, int z)
+		{	
+	 		float y;
+			y = x / z;
+		 	return(y);
+		}
+
 int main(int argc, char **argv)
 {
 	int N, i;
 	int *a;
-	float avggrd;
-	
+	float avggrd, porog;
+	//printf("Порог прохождения:");
+	//scanf("%f", &porog);
+	porog = 4.6;
 	printf("Количество оценок аттестата:");
 	scanf("%d", &N);
 	a = (int*)malloc(N * sizeof(int));
-
-	for (i = 0; i < N; i++)
-	{
-		a[i] = 2 + rand() % (5 - 2 + 1);
-		printf("%d \n", a[i]);
-	}
-
+	
+			for (i = 0; i < N; i++)
+			{
+				a[i] = 2 + rand() % (5 - 2 + 1);
+				printf("%d \n", a[i]);
+			}
 	int sum = 0;
 	for (i = 0; i < N; i++)
-	{
-		sum += a[i];
-	}
-
-	avggrd = sum / N;
+			{
+				sum += a[i];
+			}
+	
+	avggrd = faverage(sum, N);
 	printf("Средний балл аттестата: %.1f\n", avggrd);
 
-	if (avggrd > 4.6)
+	if (avggrd >= porog)
 	{
 		printf("Ученик поступит в университет\n");
 	}
